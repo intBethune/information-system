a78fa3fbae843cb90be1c2bf97dca26cb8571ed2
Your Name <youremail@yourdomain.com>
2020-12-19 23:00:20 +0800

    Signed-off-by: Your Name <youremail@yourdomain.com>

---
+++ homeworkfinal/serv/grade_stu_course_actions.py
@@ -0,0 +1,58 @@
+from urllib.parse import urlencode
+from aiohttp.web_request import Request
+import psycopg2.errors
+from aiohttp import web
+
+from .config import db_block, render_html, web_routes
+
+
+@web_routes.post('/action/cg/')
+async def  course_grade_action(request):
+    params = await request.post()
+    cou_sn = params.get("cou_sn")
+    cou_sn = int(cou_sn)
+    print(cou_sn)
+    with db_block() as db:  
+        db.execute("""
+        SELECT g.stu_sn, g.cou_sn, 
+            s.name as stu_name, 
+            c.name as cou_name, 
+            g.grade 
+        FROM course_grade as g
+            INNER JOIN student as s ON g.stu_sn = s.sn
+            INNER JOIN course as c  ON g.cou_sn = c.sn
+        WHERE cou_sn = %(cou_sn)s;
+        """,dict(cou_sn=cou_sn))
+
+        items = list(db)
+        print(items)   
+    return render_html(request, 'course_grade_list2.html',
+                       items=items)
+
+
+
+@web_routes.post('/action/sg/')
+async def  student_grade_action(request):
+    params = await request.post()
+    stu_sn = params.get("stu_sn")
+    stu_sn = int(stu_sn)
+    #print(stu_sn)
+    with db_block() as db:  
+        db.execute("""
+        SELECT g.stu_sn, g.cou_sn, 
+            s.name as stu_name, 
+            c.name as cou_name, 
+            g.grade 
+        FROM course_grade as g
+           INNER JOIN student as s ON g.stu_sn = s.sn
+           INNER JOIN course as c  ON g.cou_sn = c.sn
+        WHERE stu_sn = %(stu_sn)s;
+        """,dict(stu_sn=stu_sn))
+
+        items = list(db)
+        print(items)   
+    return render_html(request, 'student_grade_list2.html',
+                    items=items)
+
+   
+                      
